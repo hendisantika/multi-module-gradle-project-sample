5c9d48319f1108b15f5c7731a34403dc1cc5afc2
hendisantika <hendisantika@yahoo.co.id>
2025-02-08 16:13:17 +0700

    feat: Add createProduct in ProductService🫸🌀✏️📗🐧🐳⬆

--- search/src/main/java/id/my/hendisantika/search/SearchApplication.java
+++ search/src/main/java/id/my/hendisantika/search/SearchApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,9 +54,20 @@ public class SearchApplication {
     /*
       GET to return list of Products as JSON.
    */
-    @RequestMapping("/products")
+    @GetMapping("/products")
     public List<Product> products() {
         return ProductService.getProducts();
     }
 
+    /*
+        POST call to insert a new Product.
+     */
+    @PostMapping("/product/create")
+    public String createProduct(@RequestBody Product product) {
+        if (ProductService.createProduct(product)) {
+            return "Created Product = " + product.toString();
+        }
+
+        return "Creation failed for Product = " + product.toString();
+    }
 }
