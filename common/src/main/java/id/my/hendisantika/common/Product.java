f52dd73f0a683730808a9eb847cc1863876637d1
hendisantika <hendisantika@yahoo.co.id>
2025-02-08 15:41:36 +0700

    feat: Add common Project🫸🌀✏️📗🐧🐳⬆

---
+++ common/src/main/java/id/my/hendisantika/common/Product.java
@@ -0,0 +1,61 @@
+package id.my.hendisantika.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : multi-module-gradle-project-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/02/25
+ * Time: 15.40
+ * To change this template use File | Settings | File Templates.
+ */
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, String description, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
