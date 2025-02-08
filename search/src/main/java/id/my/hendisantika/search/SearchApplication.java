144beed09ee3ff8d60a533a47a02761ce98457c2
hendisantika <hendisantika@yahoo.co.id>
2025-02-08 16:07:21 +0700

    fix: Add product API🫸🌀✏️📗🐧🐳⬆

--- search/src/main/java/id/my/hendisantika/search/SearchApplication.java
+++ search/src/main/java/id/my/hendisantika/search/SearchApplication.java
@@ -1,5 +1,6 @@
 package id.my.hendisantika.search;
 
+import id.my.hendisantika.common.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,5 +22,13 @@ public class SearchApplication {
     @GetMapping("/hello")
     public String hello() {
         return "Search: Hello";
+    }
+
+    /*
+       Simple GET call to see whether we are able return a class as JSON.
+    */
+    @GetMapping("/product")
+    public Product product() {
+        return new Product(1, "Laptop", "Laptop", 45000d);
     }
 }
