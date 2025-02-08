750ffe9911f74f08ed7179ae2b3caa0d22aef73e
hendisantika <hendisantika@yahoo.co.id>
2025-02-08 16:10:31 +0700

    fix: Add ProductService🫸🌀✏️📗🐧🐳⬆

---
+++ common/src/main/java/id/my/hendisantika/common/ProductService.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.common;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : multi-module-gradle-project-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 08/02/25
+ * Time: 16.10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ProductService {
+}
