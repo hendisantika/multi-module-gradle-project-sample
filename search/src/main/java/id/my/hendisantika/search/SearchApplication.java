13d5e9ab8e90a5adefd50c4f4ec44bde4232a3f5
hendisantika <hendisantika@yahoo.co.id>
2025-02-08 16:05:14 +0700

    fix: Add search module🫸🌀✏️📗🐧🐳⬆

--- search/src/main/java/id/my/hendisantika/search/SearchApplication.java
+++ search/src/main/java/id/my/hendisantika/search/SearchApplication.java
@@ -2,12 +2,24 @@ package id.my.hendisantika.search;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/search")
 public class SearchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
     }
 
+    /*
+        Simple GET call to test this application.
+     */
+    @GetMapping("/hello")
+    public String hello() {
+        return "Search: Hello";
+    }
 }
